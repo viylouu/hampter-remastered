21f22d5e204f13906c0855eb57144174cc96a4bd
viylouu <131220600+viylouu@users.noreply.github.com>
2025-11-01 17:21:30 -0600

    a

--- src/main.c
+++ src/main.c
@@ -6,36 +6,61 @@
 #include <core/time.h>
 #include <core/renderTarget.h>
 
-int main(void) {
-    FUR_platfState* platf = fur_platf_constr();
-    FUR_renderState* render = fur_render_constr();
+FUR_platfState* platf;
+FUR_renderState* render;
+FUR_timer* time;
+FUR_renderTarget* targ;
 
+void init(void) {
+    
+}
+
+void update(void) {
+    
+}
+
+void draw(void) {
+
+render->defTarget = targ;
+
+    fur_render_clear(render, .col = (v3){.2,.4,.3});
+    fur_render_rect(render, .pos = (v2){64,64}, .size = (v2){64,64}, .col = (v4){1,0,0,1});
+
+    if (fur_input_isKeyHeld(FUR_KEY_T))
+        fur_render_tex(render, .size = (v2){64,64}, .col = (v4){1,1,1,1});
+
+render->defTarget = NULL;
+
+    fur_render_renderTarget(render, .in_target = targ);
+}
+
+void end(void) {
+    
+}
+
+int main(void) {
+    platf = fur_platf_constr();
+    render = fur_render_constr();
     fur_platf_setRender(platf, render);
 
-    FUR_timer* time = fur_makeTimer();
+    time = fur_makeTimer();
+    targ = fur_renderTarget_constr(320,180,);
 
-    FUR_renderTarget* targ = fur_renderTarget_constr(320,180,);
+    init();
 
     while (!fur_platf_shouldWindowClose(platf)) {
         fur_platf_poll(platf);
         fur_input_poll(platf);
         fur_updateTimers(&time, 1);
 
-render->defTarget = targ;
-
-        fur_render_clear(render, .col = (v3){.2,.4,.3});
-        fur_render_rect(render, .pos = (v2){64,64}, .size = (v2){64,64}, .col = (v4){1,0,0,1});
-
-        if (fur_input_isKeyHeld(FUR_KEY_T))
-            fur_render_tex(render, .size = (v2){64,64}, .col = (v4){1,1,1,1});
-
-render->defTarget = NULL;
-
-        fur_render_renderTarget(render, .in_target = targ);
+        update();
+        draw();
 
         fur_render_flush(render);
         fur_platf_present(platf);
     }   
+
+    end();
 
     fur_renderTarget_destr(targ);
 
